a3f1b1aede2c0a46545aff6b9f7b9396769696f3
Chris Bui <chrisbui188.dev@gmail.com>
2022-11-17 01:25:01 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App">This is the first time I used Vite</div>;
+}
+
+export default App;
