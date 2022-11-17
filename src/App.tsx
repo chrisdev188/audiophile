6eb00b0be2e1a0cf910ee661a4eb4d2411fc63f2
Chris Bui <chrisbui188.dev@gmail.com>
2022-11-17 01:48:58 -0500

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return <div className="App">This is the first time I used Vite</div>;
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    accent: "hsl(22,65%,57%)",
+    accentHover: "hsl(21,94%,75%)",
+    main: "hsl(0,0%,6%)",
+    card: "hsl(0,0%,95%)",
+    divider: "hsla(0,0%,59%,.2)",
+  },
+  fonts: {
+    main: "Manrope",
+  },
+} as const;
