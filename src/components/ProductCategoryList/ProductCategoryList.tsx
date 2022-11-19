8f85fc0fe2e91b2deb85fb81ed5cff7abdd3e1f2
Chris Bui <chrisbui.dev188@gmail.com>
2022-11-18 19:00:53 -0500

    Change folder structure of ProductCategoryList folder

--- src/components/ProductCategoryList/ProductCategoryList.tsx
+++ src/components/ProductCategoryList/ProductCategoryList.tsx
@@ -1,47 +1,15 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { MaxWidthContainer } from "../../globalStyles";
 import {
-  Container,
-  HeadingSix,
-  MaxWidthContainer,
-  TwelveColumnsGrid,
-} from "../../globalStyles";
+  StyledProducCategoryContainer,
+  StyledProductCategoryList,
+  CategoryItem,
+  ProductThumbnail,
+  ProductName,
+} from "./styles";
 import headphoneImg from "../../assets/shared/desktop/image-category-thumbnail-headphones.png";
 import speakerImg from "../../assets/shared/desktop/image-category-thumbnail-speakers.png";
 import earphoneImg from "../../assets/shared/desktop/image-category-thumbnail-earphones.png";
 import ButtonText from "../UI/Button/ButtonText";
-
-const StyledProducCategoryContainer = styled(Container)`
-  margin-block: 10rem;
-`;
-const StyledProductCategoryList = styled(TwelveColumnsGrid)`
-  gap: 7rem;
-  @media (min-width: 768px) {
-    gap: 2rem;
-  }
-`;
-const CategoryItem = styled.li`
-  padding: 5rem 1rem 2.5rem 1rem;
-  border-radius: 8px;
-  background-color: ${({ theme }) => theme.colors.card};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  gap: 1.5rem;
-  position: relative;
-  @media (min-width: 768px) {
-    grid-column: span 4;
-  }
-`;
-const ProductThumbnail = styled.img`
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  max-width: 10rem;
-`;
-const ProductName = styled(HeadingSix)``;
 
 const categoryList = [
   { id: 1, name: "headphones", path: "/headphones", imgSrc: headphoneImg },
---
+++ src/components/ProductCategoryList/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { Container, TwelveColumnsGrid, HeadingSix } from "../../globalStyles";
+
+export const StyledProducCategoryContainer = styled(Container)`
+  margin-block: 10rem;
+`;
+export const StyledProductCategoryList = styled(TwelveColumnsGrid)`
+  gap: 7rem;
+  @media (min-width: 768px) {
+    gap: 2rem;
+  }
+`;
+export const CategoryItem = styled.li`
+  padding: 5rem 1rem 2.5rem 1rem;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.card};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 1.5rem;
+  position: relative;
+  @media (min-width: 768px) {
+    grid-column: span 4;
+  }
+`;
+export const ProductThumbnail = styled.img`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 10rem;
+`;
+export const ProductName = styled(HeadingSix)``;
